5aa77e5d3541e55e333797adb8bdf01485989ec7
Fabián Vélez Vicente <fabian.velez1996@gmail.com>
2016-10-05 08:27:51 -0400

    Stable

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-
-app.use('/', express.static(__dirname + 'index.html'));
-
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+$(document).ready(function() {
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 });
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
+$('.parallax-window').parallax({imageSrc: 'image/whatwedo.jpg'});
